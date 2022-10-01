927b4810177846f5b08165695e5d263a0059ee82
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-10-01 23:15:08 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Type a number ");
+int num = int.Parse(Console.ReadLine());
+
+string N = Convert.ToString(num);
+
+int res = 0;
+
+for (int i=0;i<N.Length; i++)
+{
+    res += N[i]-'0';
+}
+
+Console.WriteLine(res);
