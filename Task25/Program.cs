c17932ad244bfb6b488fe4449e99a0267e2b3012
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-10-01 21:57:36 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Type number A ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Type number B ");
+int b = int.Parse(Console.ReadLine());
+
+int i;
+int res = 1;
+
+for (i=1; i<=b; i++)
+{
+    res*=a;
+}
+
+Console.WriteLine(res);
