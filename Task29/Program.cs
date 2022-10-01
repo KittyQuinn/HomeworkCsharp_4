b80b7e30a8269549d6e37c711e771d15383a450f
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-10-01 23:55:40 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 
+// 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine("Type numbers ");
+string N = Console.ReadLine();
+
+Console.WriteLine($"[{N}]");
